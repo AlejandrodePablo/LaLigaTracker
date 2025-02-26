0c634a3601f45e0bf623ceada61e3a3bf649fbc5
AlejandrodePablo <depablocorreal@gmail.com>
2025-02-26 20:34:11 +0100

    Added Remote Module to Koin

--- app/src/main/java/edu/iesam/laligatracker/app/LaLigaTrackerApp.kt
+++ app/src/main/java/edu/iesam/laligatracker/app/LaLigaTrackerApp.kt
@@ -2,6 +2,7 @@ package edu.iesam.laligatracker.app
 
 import android.app.Application
 import edu.iesam.laligatracker.app.di.AppModule
+import edu.iesam.laligatracker.app.di.RemoteModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 import org.koin.ksp.generated.module
@@ -11,7 +12,7 @@ class LaLigaTrackerApp : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@LaLigaTrackerApp)
-            modules(AppModule().module)
+            modules(AppModule().module, RemoteModule().module)
         }
     }
 }
