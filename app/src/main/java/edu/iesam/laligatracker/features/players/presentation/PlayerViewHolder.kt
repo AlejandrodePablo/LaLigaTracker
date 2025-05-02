c739df78c8a9469a6994b65e7e72598d18e9265e
AlejandrodePablo <depablocorreal@gmail.com>
2025-05-02 18:56:30 +0200

    Enhance player and club views with layout improvements

--- app/src/main/java/edu/iesam/laligatracker/features/players/presentation/PlayerViewHolder.kt
+++ app/src/main/java/edu/iesam/laligatracker/features/players/presentation/PlayerViewHolder.kt
@@ -1,5 +1,6 @@
 package edu.iesam.laligatracker.features.players.presentation
 
+import android.annotation.SuppressLint
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import edu.iesam.laligatracker.app.extensions.loadUrl
@@ -9,12 +10,15 @@ import edu.iesam.laligatracker.features.players.domain.Player
 class PlayerViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
     private lateinit var binding: ViewPlayersItemBinding
 
+    @SuppressLint("SetTextI18n")
     fun bind(player: Player) {
         binding = ViewPlayersItemBinding.bind(view)
         binding.apply {
             playerName.text = player.name
             playerImage.loadUrl(player.image)
             numberId.text = player.number
+            goals.text = "Goles: " + player.stats.amountGoals.toString()
+            assists.text = "Asistencias: " + player.stats.amountAssists.toString()
         }
     }
 }
